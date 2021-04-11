0f9729de638119bcb20c5ff3beefeb48f6fbefff
tineetinytin <christine.elene@gmail.com>
2021-04-10 23:12:43 -0700

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//FIVE FAVORITE MOVIES
+const fave = ['Moana', 'Soul', 'Inception', 'GoodFellas', 'Seaspiracy'];
+console.log(fave[1]);
+
+let movies = new Array(5);
+movies[0] = 'Moana';
+movies[1] = 'Soul';
+movies[2] = 'Inception';
+movies[3] = 'GoodFellas';
+movies[4] = 'Seaspiracy';
