55c16113e476f73732edfa3f127189a8f75bffb5
tineetinytin <christine.elene@gmail.com>
2021-04-10 23:30:25 -0700

    constructor method

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-//FIVE FAVORITE MOVIES
-const fave = ['Moana', 'Soul', 'Inception', 'GoodFellas', 'Seaspiracy'];
-console.log(fave[1]);
+//STRING ARRAY
+// let fave = ['Moana', 'Soul', 'Inception', 'GoodFellas', 'Seaspiracy'];
+// console.log(fave[1]);
 
-let movies = new Array(5);
-movies[0] = 'Moana';
-movies[1] = 'Soul';
-movies[2] = 'Inception';
-movies[3] = 'GoodFellas';
-movies[4] = 'Seaspiracy';
+//CONSTRUCTOR METHOD
+// let movies = new Array(5);
+// movies[0] = 'Moana';
+// movies[1] = 'Soul';
+// movies[2] = 'Inception';
+// movies[3] = 'GoodFellas';
+// movies[4] = 'Seaspiracy';
+// console.log(movies[0]);
+
+//CONSTRUCTOR METHOD + ADD NEW MOVIE IN 3RD POSITION
+// let movies = new Array(5);
+// movies[0] = 'Moana';
+// movies[1] = 'Soul';
+// movies[2] = 'Inception';
+// movies[3] = 'GoodFellas';
+// movies[4] = 'Seaspiracy';
+// console.log(movies[0]);
+// console.log(movies.length);
+
+//
+
+let movies = ['Moana', 'Soul', 'Inception', 'GoodFellas', 'Seaspiracy'];
